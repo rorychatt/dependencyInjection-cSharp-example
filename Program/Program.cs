5e8d38afa7be9e2ff4796971b44e77b2a511fd1e
rorychatt <pavel.rinne@gmail.com>
2024-07-15 11:54:40 +0200

    Feat: Extensions Hosting

---
+++ Program/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Solution;
+
+namespace Program;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+            var builder = Host.CreateApplicationBuilder(args);
+
+            // Register services
+            builder.Services.AddSingleton<IService, Service>();
+            builder.Services.AddSingleton<IClient, Client>();
+
+            var app = builder.Build();
+
+            // Resolve and run the client
+            var client = app.Services.GetRequiredService<IClient>();
+            client.Start();
+
+            app.Run();
+    }
+}
